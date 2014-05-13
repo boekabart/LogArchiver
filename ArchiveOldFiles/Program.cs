c722b8137ff57535c713a26450d792f2a3ebc50c
SeachangeBart <bart.de.boer@schange.com>
2014-05-13 16:25:33 +0200

    Try creating archive folder with compression

--- ArchiveOldFiles/Program.cs
+++ ArchiveOldFiles/Program.cs
@@ -1,25 +1,36 @@
 ﻿using System;
 using System.IO;
 using System.Linq;
+using System.Management;
 
 namespace ArchiveOldFiles
 {
     class Program
     {
-        static int Main(string[] args)
+        private static int Main(string[] args)
         {
-            if (args.Length<2)
+            if (args.Length < 2)
                 return Error("Usage: ArchiveOldFiles <dir> <days>");
             var dir = args[0];
             if (!Directory.Exists(dir))
                 return Error("Directory {0} does not exist", dir);
 
-            if (!Directory.Exists(Path.Combine(dir, "Archive")))
-                return Error("Directory {0}\\Archive does not exist - please create it and set its properties to 'compress'", dir);
-
             int days;
             if (!int.TryParse(args[1], out days))
                 return Error("Usage: ArchiveOldFiles <dir> <days>");
+
+            var archivePath = Path.Combine(dir, "Archive");
+
+            if (!Try(() =>
+            {
+                if (!Directory.Exists(archivePath))
+                    Directory.CreateDirectory(archivePath);
+            }))
+            {
+                return Error("Failed to create archive folder");
+            }
+
+            TrySetCompression(archivePath);
 
             var delay = TimeSpan.FromDays(days);
             var now = DateTime.UtcNow.Date;
@@ -28,10 +39,48 @@ namespace ArchiveOldFiles
             var fileInfos = dirInfo.EnumerateFiles();
             var oldFiles = fileInfos.Where(fi => (now - fi.LastWriteTimeUtc) > delay);
             foreach (var file in oldFiles)
-            {
                 MoveFileToArchive(file);
-            }
+
             return 0;
+        }
+
+        private static bool Try(Action action)
+        {
+            try
+            {
+                action();
+                return true;
+            }
+            catch (Exception e)
+            {
+                Console.Error.WriteLine("Exception: {0}", e.Message);
+                return false;
+            }
+        }
+
+        private static void TrySetCompression(string destinationDir)
+        {
+            try
+            {
+                var directoryInfo = new DirectoryInfo(destinationDir);
+                if ((directoryInfo.Attributes & FileAttributes.Compressed) == FileAttributes.Compressed)
+                {
+                    Console.WriteLine("Archive folder already compressed");
+                    return;
+                }
+                var destinationDirFixed = destinationDir.Replace(@"\", "/").TrimEnd('/');
+                var objPath = "Win32_Directory.Name=" + "\"" + destinationDirFixed + "\"";
+                using (var dirManagementObject = new ManagementObject(objPath))
+                {
+                    var outParams = dirManagementObject.InvokeMethod("Compress", null, null);
+                    var ret = (uint) (outParams.Properties["ReturnValue"].Value);
+                    Console.WriteLine("Tried to set compression on Archive folder, result code {0:X}", ret);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.Error.WriteLine("Failed to set compression on Archive folder: {0}", e.Message);
+            }
         }
 
         private static void MoveFileToArchive(FileInfo file)
